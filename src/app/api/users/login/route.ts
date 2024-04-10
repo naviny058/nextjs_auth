1d996cc4861ffa8c85c5d06273dde9f8ee2bcce7
Navin Yadav <naviny058@gmail.com>
2024-04-10 17:29:32 +0530

    introduce new service called mailtrap all mail related work is done here

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,40 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import User from '@/models/userModel'
+import bcryptjs from 'bcryptjs'
+
+connect()
+export async function POST(request: NextRequest) {
+    try {
+
+        const reqBody = await request.json()
+        const { email, password } = reqBody;
+        console.log(reqBody)
+
+        //check if user exists
+        const user = await User.findOne({ email })
+        if (!user) {
+            return NextResponse.json({ error: 'User does not exit' }, { status: 400 })
+        }
+        console.log('user exists')
+
+        //check if password is correct
+        const validPassword = await bcryptjs.compare(password, user.password)
+
+        if (!validPassword) {
+            return NextResponse.json({ error: "Invalid password" }, { status: 400 })
+        }
+        console.log(user)
+
+        //create token data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+        //create token
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
